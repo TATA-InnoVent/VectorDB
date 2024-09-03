a1b7cc95c7a0a5e198a2354b79a36e6b6d179fdd
Rohit Patil <rohitpradeeppatil@gmail.com>
2024-09-03 23:39:08 +0530

    Used another Embedding Model

--- app/utils/embeddings.py
+++ app/utils/embeddings.py
@@ -1,12 +1,25 @@
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-from transformers import RobertaTokenizer, RobertaModel
 
-tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
-model = RobertaModel.from_pretrained("microsoft/codebert-base")
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+tokenizer = AutoTokenizer.from_pretrained("jinaai/jina-embeddings-v2-base-code", trust_remote_code=True)
+model = AutoModelForMaskedLM.from_pretrained("jinaai/jina-embeddings-v2-base-code", trust_remote_code=True)
 
 def generate_embedding(code: str):
     inputs = tokenizer(code, return_tensors="pt", truncation=True, padding=True)
-    outputs = model(**inputs)
-    return outputs.last_hidden_state.mean(dim=1).detach().numpy()
+    outputs = model(**inputs, output_hidden_states=True)
+    hidden_states = outputs.hidden_states
+    last_hidden_state = hidden_states[-1]
+
+    mean_embedding = last_hidden_state.mean(dim=1).detach().numpy()
+
+    flattened_embedding = mean_embedding
+    print(flattened_embedding)
+    print(f"Shape of mean embedding before flattening: {mean_embedding.shape}")
+    print(f"Shape of flattened embedding: {flattened_embedding.shape}")
+    
+    return flattened_embedding
+
+
 
