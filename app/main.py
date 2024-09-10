55a4239facabf787b5889fc7a501b73144b58945
Nishantth1 <nishantthakre9@gmail.com>
2024-09-10 23:50:36 +0530

    Added the Frontend code with Forms for data Ingestion and retrieval

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import ingestion, retrieval
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routes from ingestion and retrieval modules
 app.include_router(ingestion.router)
